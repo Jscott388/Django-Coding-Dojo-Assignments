5f6c73ee9eb907b3ae0c69b6d332bcd187e51182
Joshua Scott <josh.scott388@gmail.com>
2016-10-13 19:17:42 -0400

    Added generate and delete

--- random_word/apps/randomWord/urls.py
+++ random_word/apps/randomWord/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^word$', views.word),
+    url(r'^reset$', views.clear),
 ]
--- random_word/apps/randomWord/views.py
+++ random_word/apps/randomWord/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
 # Create your views here.
 def index(request):
-    print("*"*50)
+    if 'counter' not in request.session:
+        request.session['counter'] = 1
     return render(request,'randomworld/index.html')
 
 def word(request):
     if request.method == 'POST':
-        print (request.POST)
-        print ('*'*50)
+        request.session['word'] = get_random_string(length=14)
+        request.session['counter'] += 1
+        return redirect('/')
     else:
         return redirect('/')
+
+def clear(request):
+    request.session['counter'] = 1
+    return redirect('/')
